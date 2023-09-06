a49b62c90732219820942ef4a2fb15cdad30f22d
Leon <leon.grass@gmx.net>
2023-09-06 14:53:15 +0200

    basic setup

--- app.vue
+++ app.vue
@@ -1,5 +1,72 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="page">
+    <Header></Header>
+    <NuxtPage></NuxtPage>
   </div>
 </template>
+
+<style>
+@font-face {
+  font-family: Nunito;
+  src: url('assets/fonts/Nunito/Nunito-VariableFont_wght.ttf');
+}
+:root {
+  --color-bg: #000;
+  --color-text: #fff;
+  --color-shadows: #555555;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Nunito, Calibri, sans-serif;
+}
+
+.page{
+  background-color: var(--color-bg);
+  color: var(--color-text);
+  height: 100vh;
+  width: 100vw;
+  overflow:auto;
+  position: relative;
+  padding-top: 13rem;
+}
+
+.headline{
+  /* font-family: Nunito; */
+}
+
+.text_big{
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+.header{
+  position:fixed;
+  top:0; 
+  width: 100vw;
+  height: auto;
+  background-color: var(--color-bg);
+}
+
+a{
+  text-decoration: none;
+  color: var(--color-text);
+}
+.site_link{
+  font-size: 1.5rem;
+  font-weight: 600;
+  padding: 0.5rem 1rem;
+  border-radius: 10px;
+  background-color: var(--color-text);
+  color: var(--color-bg);
+  text-decoration: none;
+  margin: 0 auto;
+  display: block;
+  width: 10rem;
+  text-align: center;
+  position: fixed;
+  right: 1rem;
+  top: 1rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="home_section">
+      <h1 class="headline section_headline">Concept and Vision</h1>
+      <p class="text_big">
+        At "ThreeBies," we believe that less is more when it comes to
+        self-expression. Our platform is all about distilling your essence into
+        three powerful images. No overwhelming feeds, just the three most
+        important things that define you, your lifestyle, and your perspective.
+        We're here to provide a space where quality speaks louder than quantity,
+        where every image becomes a story, and where you can express yourself in a
+        way that's uniquely you.
+      </p>
+    </div>
+    <div class="home_section">
+      <h1 class="headline section_headline">Who It's for</h1>
+      <p class="text_big">
+        "ThreeBies" welcomes individuals from all walks of life who appreciate
+        meaningful self-expression. Whether you're an artist seeking a platform to
+        showcase your creativity, a professional aiming to connect with peers, or
+        simply someone who values quality over quantity, our platform is tailored
+        to you. If you're looking to share your story in a focused and impactful
+        way, "ThreeBies" provides a canvas for you to display your essence,
+        lifestyle, and unique perspective.
+      </p>
+    </div>
+    <div class="home_section">
+      <h1 class="headline section_headline">How it works</h1>
+      <p class="text_big">
+        Using "ThreeBies" is quite simple. First, sign up and create your
+        profile. Then, carefully choose three images that represent you best –
+        your personality, lifestyle, and outlook. Once uploaded, your images
+        become the window into your world. Engage with others, explore the diverse
+        array of profiles, and find connections beyond the superficial. With
+        "ThreeBies," sharing your story is elegant, concise, and remarkably
+        impactful.
+      </p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
+  <style scoped>
+  .home_section {
+    padding: 1.5rem 2rem;
+    width: 80%;
+    margin-inline: auto;
+    text-align: center;
+    box-shadow: 0 0 12px var(--color-shadows);
+    /* border: 1px solid var(--color-text); */
+    border-radius: 10px;
+    margin-bottom: 3rem;
+  }
+  .section_headline {
+    font-size: 2rem;
+    font-weight: 800;
+    margin-bottom: 1rem;
+  }
+  </style>
+  
